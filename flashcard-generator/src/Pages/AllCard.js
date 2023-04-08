c2afd846d3e65386f3fa1ef3cca074e0a11d6e7a
RahulRSoni <114871652+RahulRSoni@users.noreply.github.com>
2023-04-08 15:12:09 +0530

    home page created

---
+++ flashcard-generator/src/Pages/AllCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllCards() {
+  return (
+    <div>AllCard</div>
+  )
+}
+
+export default AllCards
---
+++ flashcard-generator/src/Pages/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function myflashcard() {
+  return (
+    <div>Card</div>
+  )
+}
+
+export default myflashcard
